3cb6d7761697bcca46209f9256cf4359659ca194
Alexander Larson <alexanderlarson@gmail.com>
2014-03-06 16:37:50 -0700

    initial commit

---
+++ application/views/welcome_message.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Intern Burrito Timer</title>
+        
+        <!-- Our CSS stylesheet file -->
+        <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans+Condensed:300" />
+        <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css" rel="stylesheet">
+        <link rel="stylesheet" href="css/jquery.countup.css" /> <!--timer style-->
+        <link rel="stylesheet" href="css/styles.css" />
+       
+    </head>
+    
+    <body>
+		<div id="countdown"></div>
+
+  		<div id="reset_button"><a href="#" class="btn btn-danger btn-large"><i class="icon-white icon-time"></i> Reset</a></div>
+
+
+        <!-- JavaScript includes -->
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+		<script src="js/script.js"></script>
+		<script src="js/jquery.countup.js"></script>
+    
+    </body>
+</html>
